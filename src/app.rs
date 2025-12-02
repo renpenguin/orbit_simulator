22780bdf360f1fc9826ab9b98a7d21752c25e79c
Red Penguin <redpenguin777@yahoo.com>
2025-12-02 10:55:01 +0000

    reorganise `update()` function

--- src/app.rs
+++ src/app.rs
@@ -71,44 +71,121 @@ impl eframe::App for App {
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
-    #[expect(clippy::too_many_lines)]
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        // Check new key presses
         ctx.input(|i| {
             for event in &i.events {
-                match &event {
-                    // Shortcut key
-                    egui::Event::Key {
-                        key: egui::Key::Slash,
-                        pressed: true,
-                        repeat: false,
-                        modifiers,
-                        ..
-                    } if modifiers.contains(egui::Modifiers::CTRL) => {
-                        self.shortcuts_shown = !self.shortcuts_shown;
-                    }
-
-                    // Play/pause
-                    egui::Event::Key {
-                        key: egui::Key::Space,
-                        pressed: true,
-                        repeat: false,
-                        ..
-                    } => {
-                        self.simulation.playing = !self.simulation.playing;
-                    }
-
-                    _ => (),
-                }
+                self.handle_shortcut(event);
             }
         });
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
+        self.draw_top_panel(ctx);
+        self.draw_shortcuts_screen(ctx);
 
+        // let delta_time = self.last_draw.elapsed().as_secs_f64();
+        self.last_draw = Instant::now();
+
+        // Simulate planets
+        if self.simulation.playing {
+            self.simulation.handle_collisions();
+            self.simulation.simulate_gravity();
+        }
+
+        // Main planet space
+        egui::CentralPanel::default()
+            .frame(Frame::default().inner_margin(0.0).fill(Color32::BLACK))
+            .show(ctx, |ui| {
+                // Create a "canvas" for drawing on that's 100% x 300px
+                let (response, painter) =
+                    ui.allocate_painter(ui.available_size(), Sense::click_and_drag());
+
+                // Get the relative position of our "canvas"
+                let to_screen = RectTransform::from_to(
+                    Rect::from_min_size(Pos2::ZERO, response.rect.size()),
+                    response.rect,
+                );
+
+                // Handle mouse inputs
+                if response.hovered() {
+                    self.handle_sim_mouse_input(ctx, to_screen);
+                }
+
+                // Draw planets
+                for planet in self.simulation.get_planets() {
+                    painter.circle_filled(
+                        to_screen.transform_pos(planet.pos.into()),
+                        planet.radius() as f32,
+                        Color32::WHITE,
+                    );
+                }
+            });
+
+        ctx.request_repaint_after(Duration::from_secs_f32(1.0 / 60.0));
+    }
+}
+
+impl App {
+    // If the passed event is a shortcut, handle it.
+    fn handle_shortcut(&mut self, event: &egui::Event) {
+        match &event {
+            // Shortcut key
+            egui::Event::Key {
+                key: egui::Key::Slash,
+                pressed: true,
+                repeat: false,
+                modifiers,
+                ..
+            } if modifiers.contains(egui::Modifiers::CTRL) => {
+                self.shortcuts_shown = !self.shortcuts_shown;
+            }
+
+            // Play/pause
+            egui::Event::Key {
+                key: egui::Key::Space,
+                pressed: true,
+                repeat: false,
+                ..
+            } => {
+                self.simulation.playing = !self.simulation.playing;
+            }
+
+            _ => (),
+        }
+    }
+
+    // Handle mouse inputs (clicking, moving) while over the simulation area
+    fn handle_sim_mouse_input(&mut self, ctx: &egui::Context, to_screen: RectTransform) {
+        ctx.input(|i| {
+            if let Some(click_pos) = i.pointer.press_origin() {
+                // Map screen coordinates to position in painter
+                let click_pos = to_screen.inverse().transform_pos(click_pos);
+
+                if i.pointer.primary_pressed() {
+                    match self.click_mode {
+                        ClickMode::Spawn => self.simulation.spawn_planet_at(click_pos),
+                        ClickMode::Delete => {
+                            let mut planet_under_mouse = None;
+                            for (i, body) in self.simulation.get_planets().enumerate() {
+                                let is_selectable = (click_pos - Pos2::from(body.pos))
+                                    .length_sq()
+                                    < 100.0;
+                                if is_selectable {
+                                    planet_under_mouse = Some(i);
+                                    break;
+                                }
+                            }
+                            if let Some(i) = planet_under_mouse {
+                                self.simulation.planets.swap_remove(i);
+                            }
+                        }
+                        _ => println!("This will do something eventually!"),
+                    }
+                }
+            }
+        });
+    }
+
+    fn draw_top_panel(&mut self, ctx: &egui::Context) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             egui::MenuBar::new().ui(ui, |ui| {
                 ui.menu_button("File", |ui| {
                     if ui.button("New").clicked() {
@@ -168,10 +245,10 @@ impl eframe::App for App {
                 );
             });
         });
+    }
 
-        // For selected or pinned planets
-        // egui::Window::new("planet 0")
-
+    #[rustfmt::skip]
+    fn draw_shortcuts_screen(&mut self, ctx: &egui::Context) {
         // Define the size of the shortcuts window to always fill the central panel, with an 8-pixel margin.
         let mut shortcuts_rect = ctx.screen_rect();
         *shortcuts_rect.top_mut() += 24.0;
@@ -180,108 +257,24 @@ impl eframe::App for App {
         shortcuts_rect = shortcuts_rect.shrink(8.0);
 
         // Shortcuts window
-        egui::Window::new("Shortcuts Cheatsheet")
-            .fixed_rect(shortcuts_rect)
-            .collapsible(false)
-            .open(&mut self.shortcuts_shown)
-            .show(ctx, |ui| {
-                egui::ScrollArea::vertical()
-                    .auto_shrink(false)
-                    .show(ui, |ui| {
-                        egui::Grid::new("shortcuts_cheatsheet")
-                            .spacing(egui::Vec2::splat(8.0))
-                            .show(ui, |ui| {
-                                for (shortcut, description) in SHORTCUTS {
-                                    let widget_width = ui
-                                        .horizontal(|ui| {
-                                            ui.label(
-                                                egui::RichText::new(shortcut)
-                                                    .monospace()
-                                                    .size(24.0),
-                                            );
-                                            ui.label(egui::RichText::new(description).size(16.0));
-                                            ui.add_space(8.0);
-                                        })
-                                        .response
-                                        .rect
-                                        .width();
+        egui::Window::new("Shortcuts Cheatsheet").fixed_rect(shortcuts_rect).collapsible(false).open(&mut self.shortcuts_shown).show(ctx, |ui| {
+            egui::ScrollArea::vertical().auto_shrink(false).show(ui, |ui| {
+                egui::Grid::new("shortcuts_cheatsheet").spacing(egui::Vec2::splat(8.0)).show(ui, |ui| {
+                    for (shortcut, description) in SHORTCUTS {
+                        let widget_width = ui.horizontal(|ui| {
+                            ui.label(egui::RichText::new(shortcut).monospace().size(24.0));
+                            ui.label(egui::RichText::new(description).size(16.0));
+                            ui.add_space(8.0);
+                        }).response.rect.width();
 
-                                    // If there is not enough space for another widget, start a new row
-                                    if (shortcuts_rect.width() - ui.cursor().left_top().x
-                                        + shortcuts_rect.left())
-                                        < widget_width
-                                    {
-                                        ui.end_row();
-                                    }
-                                }
-                            });
-                    });
-            });
-
-        // let delta_time = self.last_draw.elapsed().as_secs_f64();
-        self.last_draw = Instant::now();
-
-        // Simulate planets
-        if self.simulation.playing {
-            self.simulation.handle_collisions();
-            self.simulation.simulate_gravity();
-        }
-
-        // Main planet space
-        egui::CentralPanel::default()
-            .frame(Frame::default().inner_margin(0.0).fill(Color32::BLACK))
-            .show(ctx, |ui| {
-                // Create a "canvas" for drawing on that's 100% x 300px
-                let (response, painter) =
-                    ui.allocate_painter(ui.available_size(), Sense::click_and_drag());
-
-                // Get the relative position of our "canvas"
-                let to_screen = RectTransform::from_to(
-                    Rect::from_min_size(Pos2::ZERO, response.rect.size()),
-                    response.rect,
-                );
-
-                if response.hovered() {
-                    ctx.input(|i| {
-                        if let Some(click_pos) = i.pointer.press_origin() {
-                            // Map screen coordinates to position in painter
-                            let click_pos = to_screen.inverse().transform_pos(click_pos);
-
-                            if i.pointer.primary_pressed() {
-                                match self.click_mode {
-                                    ClickMode::Spawn => self.simulation.spawn_planet_at(click_pos),
-                                    ClickMode::Delete => {
-                                        let mut planet_under_mouse = None;
-                                        for (i, body) in self.simulation.get_planets().enumerate() {
-                                            let is_selectable = (click_pos - Pos2::from(body.pos))
-                                                .length_sq()
-                                                < 100.0;
-                                            if is_selectable {
-                                                planet_under_mouse = Some(i);
-                                                break;
-                                            }
-                                        }
-                                        if let Some(i) = planet_under_mouse {
-                                            self.simulation.planets.swap_remove(i);
-                                        }
-                                    }
-                                    _ => println!("This will do something eventually!"),
-                                }
-                            }
+                        // If there is not enough space for another widget, start a new row
+                        let remaining_width = shortcuts_rect.width() - ui.cursor().left_top().x + shortcuts_rect.left();
+                        if remaining_width < widget_width {
+                            ui.end_row();
                         }
-                    });
-                }
-
-                // Draw planets
-                for planet in self.simulation.get_planets() {
-                    painter.circle_filled(
-                        to_screen.transform_pos(planet.pos.into()),
-                        planet.radius() as f32,
-                        Color32::WHITE,
-                    );
-                }
+                    }
+                });
             });
-
-        ctx.request_repaint_after(Duration::from_secs_f32(1.0 / 60.0));
+        });
     }
 }
